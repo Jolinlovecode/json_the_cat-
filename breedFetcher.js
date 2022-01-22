c47f061f6b2db0dce85d2cdd4e2dea937f1b45b5
Hongling Zhang <jolinhlzhang@email.com>
2022-01-22 12:29:02 -0700

    w2:d4-api

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,14 @@
 const request = require('request');
-const urlName =  "https://api.thecatapi.com/v1/breeds/search?q=sib";
+const catname = process.argv[2];
+console.log(process.argv[2]); //output sib if node breedFetcher.js sib
+const urlName =  `https://api.thecatapi.com/v1/breeds/search?q=${catname}`; //https://api.thecatapi.com/v1/breeds/search?q=$sib
 
 request(urlName, (error,response, body)=> {
   const data = JSON.parse(body);
   console.log(data);
   console.log(typeof data);
   console.log(data[0].description);
+  console.error('error:',error); 
 });
 
 
